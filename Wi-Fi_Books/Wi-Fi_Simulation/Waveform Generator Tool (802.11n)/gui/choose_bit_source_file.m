918e3991af3a9aab127e613c840bc682910afc72
Fangxin Xu <fzxy002763@gmail.com>
2022-01-21 00:20:46 +0800

    Simulation_info_update
    Simulation_info_update

--- Wi-Fi_Books/Wi-Fi_Simulation/Waveform Generator Tool (802.11n)/gui/choose_bit_source_file.m
+++
@@ -1,9 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%       Written in Metalink Broadband
-%       www.metalinkbb.com
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-[fileName,data_dir] = uigetfile({ '*.*',  'All Files (*.*)'}, ...
-    'Select Input File','data\')
-str_set('bit_source',[data_dir fileName]);
--- Wi-Fi_Books/Wi-Fi_Simulation/Waveform Generator Tool (802.11n)/gui/save_output.m
+++
@@ -1,9 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%       Written in Metalink Broadband
-%       www.metalinkbb.com
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% save output
-sim80211nconfig=get_gui_params;
-sig=tx_n_highlevel(sim80211nconfig);
-save txn_out sim80211nconfig sig
